c188665d647c78ab4872213655931c105ee5b338
Wilksonccp <wilkson.cc@gmail.com>
2024-05-25 10:24:52 -0300

    aprendi sobre como usar os metodos'get' e 'set'o

--- ProbelmaExemplo01/Produto.cs
+++ ProbelmaExemplo01/Produto.cs
@@ -4,42 +4,64 @@ namespace ProblemaExemplo02;
 
 public class Produto
 {
-    public string Nome;
-    public double Preco;
-    public int Quantidade;
+    private string _nome;
+    private double _preco;
+    private int _quantidade;
 
     public Produto(string nome, double preco, int quantidade)
     {
-        Nome = nome;
-        Preco = preco;
-        Quantidade = quantidade;
+        _nome = nome;
+        _preco = preco;
+        _quantidade = quantidade;
 
     }
 
     public Produto(string nome, double preco)
     {
-        Nome = nome;
-        Preco = preco;
-        Quantidade = 8;
+        _nome = nome;
+        _preco = preco;
+        _quantidade = 8;
     } 
 
 
+    public string GetNome()
+    {
+        return _nome;
+    }
+    public void SetNome(string nome)
+    {
+        while (nome.Length < 2 || nome == null)
+        {
+            Console.WriteLine("insira um valor válido para o nome do produto");
+            nome = Console.ReadLine();
+        }
+        _nome = nome;
+    }
+    public double GetPreco()
+    {
+        return _preco;
+    }
+    public int GetQuantidade()
+    {
+        return _quantidade;
+    }
+
     public double ValorTotalEmEstoque()
     {
-        return Preco * Quantidade;
+        return _preco * _quantidade;
     }
     public void AdicionaProduto(int quantidade)
     {
-        Quantidade += quantidade;
+        _quantidade += quantidade;
     }
     public void RemoveProduto(int quantidade)
     {
-        Quantidade -= quantidade;
+        _quantidade -= quantidade;
     }
 
     public override string ToString()
     {
         CultureInfo cultura = CultureInfo.InvariantCulture;
-        return $"Dados do Produto: {Nome}, R$ {Preco.ToString("f2", cultura)}, {Quantidade} unidades, Total R$ {ValorTotalEmEstoque().ToString("f2", cultura)}";
+        return $"Dados do Produto: {_nome}, R$ {_preco.ToString("f2", cultura)}, {_quantidade} unidades, Total R$ {ValorTotalEmEstoque().ToString("f2", cultura)}";
     }
 }
--- ProbelmaExemplo01/Program.cs
+++ ProbelmaExemplo01/Program.cs
@@ -7,37 +7,21 @@ namespace ProblemaExemplo02
     {
         static void Main(string[] args)
         {
-            // Produto p = new Produto("TV", 900.00, 12);
+            CultureInfo cultura = CultureInfo.InvariantCulture;
+            Produto a = new Produto("Caneca", 15.00, 20 );
 
-            Console.WriteLine("Entre com os dados do produto");
-            Console.Write("Produto: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no estoque: ");
-            // int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine( a.ToString() );
+            Console.WriteLine( a.GetNome() );
+            Console.WriteLine(  );
 
-            Produto p = new Produto(nome, preco);
+            Console.WriteLine( "Insira um novo nome para o 'produto'" );
+            string NovoNome = Console.ReadLine();
+            a.SetNome( NovoNome );
 
-            // Console.WriteLine($"Dados do produto: {p.Nome}, R$ {p.Preco}, {p.Quantidade} unidades, TOTAL R$ {p.ValorTotalEmEstoque()}");
-            Console.WriteLine(p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque:");
-            int Qtd = int.Parse(Console.ReadLine());
-            p.AdicionaProduto(Qtd);
-            Console.WriteLine($"Dados atualizados: {p}");
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser removidos ao estoque:");
-            Qtd = int.Parse(Console.ReadLine());
-            p.RemoveProduto(Qtd);
-            Console.WriteLine($"Dados atualizados: {p}");
-            Console.WriteLine();
-
-
-
-
+            Console.WriteLine( a.ToString() );
+            Console.WriteLine( a.GetNome() );
+            Console.WriteLine( a.GetPreco().ToString("f2", cultura) );
+            Console.WriteLine( a.GetQuantidade() );
         }
     }
 }
